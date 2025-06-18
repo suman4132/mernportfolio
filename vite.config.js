693c3ae4ee384e7f9298fa4f83edab1261ce3285
suman4132 <sk8760053@gmail.com>
2025-06-18 11:44:57 +0530

    Updated features

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/suman4132/mernportfolio.git', // Replace with your repo name (must match GitHub Pages URL)
+  base: '/mernportfolio/', // ✅ Add this line
 });
